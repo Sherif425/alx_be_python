721648409cc075af4659efc4a37152284f0c0c2e
Sherif Mohamed <sherif425@gmail.com>
2025-08-10 15:29:11 +0300

    creating a system that models a library with different types of books.

---
+++ oop/library_system.py
@@ -0,0 +1,41 @@
+class Book:
+    def __init__(self, title, author):
+        """Base Book class with title and author."""
+        self.title = title
+        self.author = author
+        
+    def __str__(self):
+        """Return string representation for a regular Book."""
+        return f"Book: {self.title} by {self.author}"
+        
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        """EBook inherits from Book and adds file_size."""
+        super().__init__(title, author)
+        self.file_size = file_size
+        
+    def __str__(self):
+        return f"EBook: {self.title} by by {self.author}, File Size: {self.file_size}KB"
+        
+        
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+        
+    def __str__(self):
+        """Return string representation for a PrintBook."""
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+    
+class Library:
+    def __init__(self):
+        self.books = []
+        
+    def add_book(self, book: Book):
+        self.books.append(book)
+    
+    def list_books(self):
+        for book in self.books:
+            print(book)    
+    
