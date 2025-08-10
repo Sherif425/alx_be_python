9b34c6263049c961c2963f230b58926841ba0cc1
Sherif Mohamed <sherif425@gmail.com>
2025-08-10 14:41:40 +0300

    implementing a Book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,1 +1,20 @@
 
+class Book:
+    def __init__(self, title, author, year):
+        """Constructor: Initializes the Book instance."""
+        self.title = title
+        self.author = author
+        self.year = year
+        
+    def __del__(self):
+        """Destructor: Called when the object is deleted."""
+        print(f"Deleting {self.title}")
+        
+    def __str__(self):
+        """String representation: Used by print() and str()."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
+    def __repr__(self):
+        """Official representation: Should recreate the object."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    
